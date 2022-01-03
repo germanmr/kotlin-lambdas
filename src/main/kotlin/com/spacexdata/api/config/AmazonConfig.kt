8a73a799079b5cceb1994a1d8dde45e24c965632
German Munoz Romano <german.munioz.romano@gmail.com>
2022-01-03 16:33:12 +0100

    Profiles enum was moved to a new directory

--- src/main/kotlin/com/spacexdata/api/config/AmazonConfig.kt
+++ src/main/kotlin/com/spacexdata/api/config/AmazonConfig.kt
@@ -3,7 +3,7 @@ package com.spacexdata.api.config
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3Client
-import com.spacexdata.api.notificationappkotlin.config.Profiles.TEST
+import com.spacexdata.api.config.Profiles.TEST
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
